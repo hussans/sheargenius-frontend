1ca0a05e65b4b3602c1900a28f188730284a7915
hussans <uwnhasan@gmail.com>
2025-03-25 13:49:18 -0700

    New Commit Test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,7 @@ export default function Home() {
     <div className="bg-white">
       <Navbar />
       <div className="flex flex-col gap-1 mb-1 text-center px-10">
-        <Link
-          className="bg-slate-300 p-4 hover:bg-black hover:text-white active:bg-slate-200 active:text-black" href={"./login"}> Login </Link>
+        <Link className="bg-slate-300 p-4 hover:bg-black hover:text-white active:bg-slate-200 active:text-black" href={"./login"}> Login </Link>
         <Link className="bg-slate-300 p-4 hover:bg-black hover:text-white active:bg-slate-200 active:text-black" href={"./register"}> Register </Link>
       </div>
     </div>
