bdce707ace0350ac160bd65505db4a5554b49afc
hussans <uwnhasan@gmail.com>
2025-04-01 10:07:01 -0700

    Barber Essentials Section for Home Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -50,6 +50,47 @@ export default function Home() {
       <section className="mt-44">
         <CreateAccPopup />
       </section>
+      <section className="mt-25 px-5">
+        <div className="grid grid-cols-2 grid-rows-2 gap-3">
+          <div className="relative col-span-2">
+            <img className="w-full h-[600px] object-cover" src="./barberessentials.png" alt="Barber Essentials Background Image" />
+            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col items-center">
+              <img className="w-[50px]" src="./icons/sheargenius-essentials.svg" alt="ShearGenius Logo" />
+              <div className="flex justify-center flex-col items-center mb-10 mt-5">
+                <p className="font-[NeueMontreal-Medium] text-white text-5xl"> Barber Essentials </p>
+                <p className="font-[NeueMontreal-Medium] text-white text-sm"> Create the best toolbox for success </p>
+              </div>
+              <button className="font-[NeueMontreal-Medium] text-black bg-white px-25 py-2 hover:bg-[#FFFFFF1A] hover:outline-2 hover:outline-white hover:text-white active:text-black active:bg-white active:outline-0 cursor-pointer transition-all duration-75"> 
+                EXPLORE 
+              </button>
+            </div>
+          </div>
+          <div className="relative">
+            <img className="w-full h-[600px] object-cover" src="./barberetiquette.jpg" alt="Barber Shop Cutting Area Background" />
+            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col items-center gap-7">
+              <div className="flex justify-center flex-col items-center text-center gap-1">
+                <p className="font-[NeueMontreal-Medium] text-white text-5xl"> Barber Shop <br /> Etiquette </p>
+                <p className="font-[NeueMontreal-Medium] text-white text-sm"> Make your next visit a breeze </p>
+              </div>
+              <button className="font-[NeueMontreal-Medium] text-black bg-white px-25 py-2 hover:bg-[#FFFFFF1A] hover:outline-2 hover:outline-white hover:text-white active:text-black active:bg-white active:outline-0 cursor-pointer transition-all duration-75"> 
+                LEARN MORE 
+              </button>
+            </div>
+          </div>
+          <div className="relative">
+            <img className="w-full h-[600px] object-cover" src="./barberitems.jpg" alt="Barber Clippers and Items Background" />
+            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col items-center gap-7">
+              <div className="flex justify-center flex-col items-center text-center gap-1">
+                <p className="font-[NeueMontreal-Medium] text-white text-5xl"> Clippers <br /> Crash Course </p>
+                <p className="font-[NeueMontreal-Medium] text-white text-sm"> Learn basic terminology </p>
+              </div>
+              <button className="font-[NeueMontreal-Medium] text-black bg-white px-25 py-2 hover:bg-[#FFFFFF1A] hover:outline-2 hover:outline-white hover:text-white active:text-black active:bg-white active:outline-0 cursor-pointer transition-all duration-75">
+                BEGIN
+              </button>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -7,9 +7,9 @@ const Navbar = () => {
     <nav className="bg-white text-black text-sm font-[NeueMontreal-Medium] border-b-2">
         <div className="max-w-[100%] mx-auto px-10">
             <div className="flex items-center justify-between h-16">
-                <div className="flex items-center ">
-                    <div className="flex-shrink-0">
-                        <a href="/" > LOGO </a>
+                <div className="flex items-center flex-row">
+                    <div className="flex items-center gap-1.5 flex-shrink-0">
+                        <a href="/" ><img className="w-[33px]" src="./icons/sheargenius-logo.svg" alt="ShearGenius Logo" /></a>
                         <a href="/"> SHEARGENIUS </a>
                     </div>
                 </div>
