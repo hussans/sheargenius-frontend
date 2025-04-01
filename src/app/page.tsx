72f93173750f1e0bf8c0a38b3d1db4331e1f8f8f
hussans <uwnhasan@gmail.com>
2025-04-01 11:13:19 -0700

    Header in a Div, Transition Header to a Component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,11 +10,13 @@ export default function Home() {
   return (
     <div className="bg-white min-h-screen w-full">
       <Navbar />
-      <header className="relative">
-        <img className="w-full h-[724px] object-cover" src="./sheargenius-banner.png" alt="Barber Shop Leather Chair Banner Image" />
-        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col items-center">
-          <h1 className="font-[NeueMontreal-Medium] text-[#FFFD71] text-8xl"> ShearGenius </h1>
-          <p className="font-[NeueMontreal-Medium] text-white text-xl"> A Hub For All Things Hair </p>
+      <header>
+        <div className="relative">
+          <img className="w-full h-[724px] object-cover" src="./sheargenius-banner.png" alt="Barber Shop Leather Chair Banner Image" />
+          <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col items-center">
+            <h1 className="font-[NeueMontreal-Medium] text-[#FFFD71] text-8xl"> ShearGenius </h1>
+            <p className="font-[NeueMontreal-Medium] text-white text-xl"> A Hub For All Things Hair </p>
+          </div>
         </div>
       </header>
       <section className="mt-20 mx-5">
