f231a43360ef555d1cc04cc5ea11bd1d60d06689
hussans <uwnhasan@gmail.com>
2025-04-04 11:41:33 -0700

    Removed Footer from Layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Footer from "@/components/ui/Footer";
 
 export const metadata: Metadata = {
   title: "ShearGenius",
