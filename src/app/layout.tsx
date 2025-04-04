53ea717131ed84f435ee9de3009455dc8609ef3e
hussans <uwnhasan@gmail.com>
2025-04-04 11:30:26 -0700

    Use Client in Footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Footer from "@/components/ui/footer";
+import Footer from "@/components/ui/Footer";
 
 export const metadata: Metadata = {
   title: "ShearGenius",
