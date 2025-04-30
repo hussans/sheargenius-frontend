b66cc6716947f6f5838cd772189689a9f32b1209
hussans <uwnhasan@gmail.com>
2025-04-30 14:45:11 -0700

    Rating Card Styling - Profile Username Show, Responsiveness, and Gold Stars with Submit Button

--- src/components/RatingComponent.tsx
+++ src/components/RatingComponent.tsx
@@ -1,33 +1,40 @@
 import Image from "next/image";
 import React, { useState } from "react";
 
-const RatingComponent = () => {
+interface RatingComponentProps {
+  usernameToRate: string;
+}
+
+const RatingComponent = ({ usernameToRate }: RatingComponentProps) => {
   const [selectedIndex, setSelectedIndex] = useState<number | null>(null);
-  const images = Array(5).fill(null); // Adjust count as needed
-  console.log(name)
+  const images = Array(5).fill(null);
 
   return (
-    <div className="px-10 pb-10 pt-3">
-      <h2 className="font-[NeueMontreal-Medium] text-center text-2xl mb-10">
-        Rate
+    <div className="px-4 sm:px-6 md:px-10 pb-8 sm:pb-10 pt-3">
+      <h2 className="font-[NeueMontreal-Medium] text-center text-xl sm:text-2xl">
+        Rate {usernameToRate}
       </h2>
-      <div className="flex justify-center gap-5 place-items-center h-[50vh]">
+      <div className="flex justify-center gap-3 sm:gap-5 items-center min-h-[150px]">
         {images.map((_, index) => (
           <Image
             key={index}
             src={
               index <= (selectedIndex ?? -1)
-                ? "/icons/star.png"
+                ? "/icons/star-gold.png"
                 : "/icons/star-empty.png"
             }
             alt={`Rating ${index + 1}`}
             onClick={() => setSelectedIndex(index)}
-            className="cursor-pointer w-12"
-            width={100}
-            height={100}
+            className="cursor-pointer w-8 h-8 sm:w-10 sm:h-10 md:w-12 md:h-12"
+            width={50}
+            height={50}
           />
         ))}
       </div>
+      <button 
+      className="bg-black w-full text-white font-[NeueMontreal-Medium] py-5 rounded-lg hover:bg-gray-200 hover:outline-2 hover:text-black active:bg-black active:text-white active:outline-0 cursor-pointer transition-all duration-75">
+        Submit
+      </button>
     </div>
   );
 };
